8485d3588c817ee0311650fc8163956e6f88600d
pedroandriottii <phab@cesar.school>
2023-06-16 01:36:43 -0300

    Homepage Added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    product_name = models.CharField(max_length=200, null=True, blank=True)
+    product_description = models.CharField(max_length=260, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
-# Create your views here.
+def home(request):
+    context = {'home' : True}
+    return render(request, 'app/index.html', context)
+
+def productList(request):
+    products = Product
+    context = {'products' : products}
+    return render(request, 'app/productList.html', context)
