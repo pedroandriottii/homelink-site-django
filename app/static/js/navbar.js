ac489be8a7cb3382404e27d1f41c69e7c24b4caa
luis <luismingati@gmail.com>
2023-06-21 16:01:46 -0300

    navbar

---
+++ app/static/js/navbar.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-icon').addEventListener('click', function () {
+  this.classList.toggle('active');
+  document.getElementById('navbar-links').classList.toggle('active');
+});
